cc64ed839f3af6e8603de486aefc72c76d1e4f3d
khairulazman67 <azman.khairul67@gmail.com>
2024-01-18 00:25:50 +0700

    render list

--- src/components/fragments/CardProduct.jsx
+++ src/components/fragments/CardProduct.jsx
@@ -4,7 +4,7 @@ import Button from '../elements/buttons/index';
 const CardProduct = (props)=>{
     const {children} = props
     return(
-        <div className="w-full max-w-sm bg-gray-800 border border-gray-200 rounded-ls shadow">
+        <div className="w-full max-w-sm bg-gray-800 border border-gray-200 rounded-ls shadow mx-3 flex flex-col justify-between">
             {children}
         </div>
     )
@@ -25,7 +25,7 @@ const Header = (props)=>{
 const Body = (props)=>{
     const {children, title} =  props
     return(
-        <div className="px-5 pb-5">
+        <div className="px-5 pb-5 h-full">
             <a href="">
                 <h5 className="text-xl font-semibold tracking-tight text-white">
                     {title}
@@ -37,11 +37,11 @@ const Body = (props)=>{
 }
 
 const Footer = (props) =>{
-    const {price, btnText} = props
+    const {price} = props
     return(
         <div className="flex items-center justify-between px-5 pb-5">
             <span className="text-xl font-bold text-white">{price}</span>
-            <Button classname="bg-blue-600">{btnText}</Button>
+            <Button classname="bg-blue-600">Add To Card</Button>
         </div>
     )
 }
--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -1,15 +1,44 @@
 import CardProduct from "../components/fragments/CardProduct"
 
+const products = [
+    {
+        id          : 1,
+        name        : "Sepatu Baru Alhamdulillah",
+        price       : "Rp. 1.000.000",
+        image       : "https://img.freepik.com/free-photo/fashion-shoes-sneakers_1203-7529.jpg?w=1800&t=st=1705508532~exp=1705509132~hmac=621962abb9e3b381da2ffc3a7199cfa1956005eb4ec95f4d94ce8ad3be513e88",
+        description : `Aliquip aliqua anim dolore dolore ex culpa voluptate aliqua mollit voluptate nisi consectetur. Ea sit veniam esse non deserunt 
+            in quis nostrud. Est dolor ut id do eiusmod officia. `
+    },
+    {
+        id          : 2,
+        name        : "Sepatu Lama Alhamdulillah",
+        price       : "Rp. 1.000.000",
+        image       : "https://img.freepik.com/free-photo/fashion-shoes-sneakers_1203-7529.jpg?w=1800&t=st=1705508532~exp=1705509132~hmac=621962abb9e3b381da2ffc3a7199cfa1956005eb4ec95f4d94ce8ad3be513e88",
+        description : `Aliquip aliqua anim Commodo duis labore sit aute amet cillum nulla et. Voluptate culpa anim aliquip irure. 
+        Aliqua labore officia sunt minim  `
+    },
+
+    {
+        id          : 3,
+        name        : "Sepatu Lama Astaghfirullah",
+        price       : "Rp. 2.000.000",
+        image       : "https://img.freepik.com/free-photo/fashion-shoes-sneakers_1203-7529.jpg?w=1800&t=st=1705508532~exp=1705509132~hmac=621962abb9e3b381da2ffc3a7199cfa1956005eb4ec95f4d94ce8ad3be513e88",
+        description : `Aliquip aliqua anim  eiusmod officia. Commodo duis labore sit aute amet cillum nulla et `
+    }
+]
+
 const ProductPage = ()=>{
     return(
         <div className="flex justify-center">
-            <CardProduct>
-                <CardProduct.Header image="https://img.freepik.com/free-photo/fashion-shoes-sneakers_1203-7529.jpg?w=1800&t=st=1705508532~exp=1705509132~hmac=621962abb9e3b381da2ffc3a7199cfa1956005eb4ec95f4d94ce8ad3be513e88"></CardProduct.Header>
-                <CardProduct.Body  title="Sepatu Baru Alhamdulillah"> 
-                    Aliquip aliqua anim dolore dolore ex culpa voluptate aliqua mollit voluptate nisi consectetur. Ea sit veniam esse non deserunt in quis nostrud. Est dolor ut id do eiusmod officia. Commodo duis labore sit aute amet cillum nulla et. Voluptate culpa anim aliquip irure. Aliqua labore officia sunt minim commodo commodo eiusmod in amet qui. Ea id laborum sunt cupidatat.    
-                </CardProduct.Body>
-                <CardProduct.Footer price="Rp. 1.000.000" btnText="Add To Card"></CardProduct.Footer>
-            </CardProduct>
+            {products.map((product,i) => (
+                <CardProduct key={i}>
+                    <CardProduct.Header image={product.image}></CardProduct.Header>
+                    <CardProduct.Body  title={product.name}> 
+                        {product.description}   
+                    </CardProduct.Body>
+                    <CardProduct.Footer price={product.price}></CardProduct.Footer>
+                </CardProduct>
+            ))}
         </div>
     )
 }
